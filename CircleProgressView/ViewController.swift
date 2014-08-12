6e07e5ec5382a1a130d620b6fa7ead2cd13cc9c0
Eric Rolf <erolf@cardinalsolutions.com>
2014-08-12 14:07:02 -0400

    Initial Commit

---
+++ CircleProgressView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircleProgressView
+//
+//  Created by Eric Rolf on 8/12/14.
+//  Copyright (c) 2014 Eric Rolf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
