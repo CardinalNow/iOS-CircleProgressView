8b13d93c4c06edcee48696ef1c3408ca76492ce6
Eric Rolf <erolf@cardinalsolutions.com>
2014-08-13 08:21:15 -0400

    Demo Changes.

--- CircleProgressView/ViewController.swift
+++ CircleProgressView/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var circleProgressView: CircleProgressView!
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var progressSlider: UISlider!
+    @IBOutlet weak var clockwiseSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.clockwiseSwitch.setOn(self.circleProgressView.clockwise, animated: false)
+        self.progressSlider.value = Float(self.circleProgressView.progress)
+        self.progressLabel.text = NSString(format:"%.3f", self.circleProgressView.progress)
+
+        //        delay(5.0) {
+        //            self.circleProgressView.progress = 0.75
+        //        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - IBActions
 
+    @IBAction func sliderDidChangeValue(sender: AnyObject) {
+        let slider:UISlider = sender as UISlider
+        self.circleProgressView.progress = Double(slider.value)
+        self.progressLabel.text = NSString(format:"%.3f", slider.value)
+    }
+    
+    @IBAction func switchDidChangeValue(sender: AnyObject) {
+        let mySwitch:UISwitch = sender as UISwitch
+        self.circleProgressView.clockwise = mySwitch.on
+        self.circleProgressView.progress = self.circleProgressView.progress
+    }
+
+    // MARK: - Helpers
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(), closure)
+    }
 }
 
