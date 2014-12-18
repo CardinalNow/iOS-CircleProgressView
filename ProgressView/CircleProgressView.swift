9060b5ca5e5949de9cf99703d342e9e700a3aa31
Ertan Ünver <ertanunver@users.noreply.github.com>
2014-12-18 17:05:21 +0200

    Fix multiple instance bug

--- ProgressView/CircleProgressView.swift
+++ ProgressView/CircleProgressView.swift
@@ -7,86 +7,85 @@
 //
 
 import UIKit
-import QuartzCore
 
-@IBDesignable
-
-@objc class CircleProgressView: UIView {
+@IBDesignable class CircularProgressView: UIView {
 
     internal struct Constants {
-        static let circleDegress = 360.0
-        static let minimumValue = 0.000001
-        static let maximumValue = 0.999999
-        static let ninetyDegrees = 90.0
-        static let twoSeventyDegrees = 270.0
-        static var contentView:UIView = UIView()
-        static var contentContainer:UIView = UIView()
+        let circleDegress = 360.0
+        let minimumValue = 0.000001
+        let maximumValue = 0.999999
+        let ninetyDegrees = 90.0
+        let twoSeventyDegrees = 270.0
+        var contentView:UIView = UIView()
+        var contentContainer:UIView = UIView()
     }
-    
-    @IBInspectable var progress: Double = Constants.minimumValue {
+
+    let constants = Constants()
+
+    @IBInspectable var progress: Double = 0.000001 {
         didSet { setNeedsDisplay() }
     }
 
     @IBInspectable var clockwise: Bool = true {
         didSet { setNeedsDisplay() }
     }
-    
+
     @IBInspectable var trackWidth: CGFloat = 10 {
         didSet { setNeedsDisplay() }
     }
-    
+
     @IBInspectable var trackImage: UIImage? {
         didSet { setNeedsDisplay() }
     }
-    
+
     @IBInspectable var trackBackgroundColor: UIColor = UIColor.grayColor() {
         didSet { setNeedsDisplay() }
     }
-    
+
     @IBInspectable var trackFillColor: UIColor = UIColor.blueColor() {
         didSet { setNeedsDisplay() }
     }
-    
+
     @IBInspectable var trackBorderColor:UIColor = UIColor.clearColor() {
         didSet { setNeedsDisplay() }
     }
-    
+
     @IBInspectable var trackBorderWidth: CGFloat = 0 {
         didSet { setNeedsDisplay() }
     }
-    
+
     @IBInspectable var centerFillColor: UIColor = UIColor.whiteColor() {
         didSet { setNeedsDisplay() }
     }
-    
+
     @IBInspectable var contentView: UIView {
-        return Constants.contentView
+        return self.constants.contentView
     }
 
     required override init(frame: CGRect) {
         super.init(frame: frame)
         self.addSubview(contentView)
     }
-    
+
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         self.addSubview(contentView)
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
         self.addSubview(contentView)
     }
-    
-    override func drawRect(var rect: CGRect) {
+
+    override func drawRect(rect: CGRect) {
         
         super.drawRect(rect)
-
+        
         let innerRect = CGRectInset(rect, trackBorderWidth, trackBorderWidth)
-    
-        progress = (progress/1.0) == 0.0 ? Constants.minimumValue : progress
-        progress = (progress/1.0) == 1.0 ? Constants.maximumValue : progress
-        progress = clockwise ? (-Constants.twoSeventyDegrees + ((1.0 - progress) * Constants.circleDegress)) : (Constants.ninetyDegrees - ((1.0 - progress) * Constants.circleDegress))
+        
+        progress = (progress/1.0) == 0.0 ? constants.minimumValue : progress
+        progress = (progress/1.0) == 1.0 ? constants.maximumValue : progress
+        progress = clockwise ? (-constants.twoSeventyDegrees + ((1.0 - progress) * constants.circleDegress)) : (constants.ninetyDegrees - ((1.0 - progress) * constants.circleDegress))
         
         let context = UIGraphicsGetCurrentContext()
         
@@ -100,14 +99,14 @@ import QuartzCore
             trackBorderColor.setStroke()
             circlePath.stroke()
         }
-    
+        
         // progress Drawing
         let progressPath = UIBezierPath()
         let progressRect: CGRect = CGRectMake(innerRect.minX, innerRect.minY, CGRectGetWidth(innerRect), CGRectGetHeight(innerRect))
         let center = CGPointMake(progressRect.midX, progressRect.midY)
         let radius = progressRect.width / 2.0
-        let startAngle:CGFloat = clockwise ? CGFloat(-progress * M_PI / 180.0) : CGFloat(Constants.twoSeventyDegrees * M_PI / 180)
-        let endAngle:CGFloat = clockwise ? CGFloat(Constants.twoSeventyDegrees * M_PI / 180) : CGFloat(-progress * M_PI / 180.0)
+        let startAngle:CGFloat = clockwise ? CGFloat(-progress * M_PI / 180.0) : CGFloat(constants.twoSeventyDegrees * M_PI / 180)
+        let endAngle:CGFloat = clockwise ? CGFloat(constants.twoSeventyDegrees * M_PI / 180) : CGFloat(-progress * M_PI / 180.0)
         
         progressPath.addArcWithCenter(center, radius:radius, startAngle:startAngle, endAngle:endAngle, clockwise:!clockwise)
         progressPath.addLineToPoint(CGPointMake(progressRect.midX, progressRect.midY))
@@ -134,6 +133,7 @@ import QuartzCore
         let layer = CAShapeLayer()
         layer.path = centerPath.CGPath
         contentView.layer.mask = layer
+        
     }
-    
+
 }
