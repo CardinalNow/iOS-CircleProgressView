ce864edfddac64395c29ea1eb4e939f53010dec7
王顺 <360752546@qq.com>
2017-01-20 06:33:12 +0800

    Support Carthage (#42)
    * Support Carthage
    
    * Update .gitignore
    
    * Bump version to 1.1.0

---
+++ CircleProgressView/CircleProgressView.h
@@ -0,0 +1,19 @@
+//
+//  CircleProgressView.h
+//  CircleProgressView
+//
+//  Created by Wang,Shun on 31/12/2016.
+//  Copyright © 2016 Eric Rolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircleProgressView.
+FOUNDATION_EXPORT double CircleProgressViewVersionNumber;
+
+//! Project version string for CircleProgressView.
+FOUNDATION_EXPORT const unsigned char CircleProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircleProgressView/PublicHeader.h>
+
+
